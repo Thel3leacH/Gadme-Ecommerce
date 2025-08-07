3a0a87d03ccf523a6eaec86f5792e645a9a71702
Thel3leacH <jiradech3007@gmail.com>
2025-08-07 16:03:15 +0700

    update(nav): add HOME

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a>
-          <img />
-        </a>
-        <a>
-          <img />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="bg-red-400 flex justify-end text-2xl font-bold shadow-md">
+      Home
+    </div>
   );
-}
+};
 
 export default App;
