07d1de82dfb8b275f1a6537b6653fa1162f5e0a0
G0lf-Thanap0n <thanaponphunthawi@gmail.com>
2025-08-08 10:44:59 +0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ const App = () => {
   return (
     <div className="min-w-screen bg-teal-500 flex justify-end gap-20 shadow-md">
       <h1 className="text-xl font-bold text-white hover:text-green-800 cursor-pointer">
+        Hello test ✌😉
+      </h1>
+      <h1 className="text-xl font-bold text-white hover:text-green-800 cursor-pointer">
         Home
       </h1>
       <h1 className="text-xl font-bold text-white hover:text-green-800 cursor-pointer">
