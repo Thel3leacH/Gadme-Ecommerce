a9d029ae55e4779d58b5c7937cb314b550343f4f
eve.cs <eve.chunyanan.s@gmail.com>
2025-08-11 10:56:26 +0700

    fix logo to appear but still too big

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,27 @@ export function Navbar() {
     return (
         <div className="bg-[#006A71]">
             <nav className="bg-[#006A71] flex flex-row justify-between">
-                <img src="../images/Gadme-Logo.png" alt="gadme-logo" />
+                <img src="/Gadme.png" alt="gadme-logo" className="w-xs h-xs" />
                 <h1>GadMe</h1>
-                <ul className="flex flex-row-reverse">
+                <ul className="flex flex-row flex-end gap-2">
                     <li >
                         <a
                             href="#">
-                            <BsSearch />
+                            <BsSearch className="text-2xl " />
                         </a>
                     </li>
 
                     <li >
                         <a
                             href="#">
-                            <BsBasket />
+                            <BsBasket className="text-2xl " />
                         </a>
                     </li>
 
                     <li >
                         <a
                             href="#">
-                            <BsPersonFill />
+                            <BsPersonFill className="text-2xl " />
                         </a>
                     </li>
                 </ul>
