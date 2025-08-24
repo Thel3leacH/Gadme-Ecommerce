f38a7453777dc95f751896517fdc77fbbebcfe9d
eve.cs <eve.chunyanan.s@gmail.com>
2025-08-25 00:01:20 +0700

    clear up the conflict in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
 import { BsBasket, BsPersonFill, BsSearch } from "react-icons/bs";
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LoginForm } from "./LoginForm";
 import { SignupForm } from "./SignupForm";
-import { SignupForm, SignupPopup } from "./SignupForm";
 import Searchbar from "./Searchbar";
 
 
@@ -26,6 +24,7 @@ const Navbar = () => {
         setIsLoginVisible(!isLoginVisible);
         setIsSignupVisible(!isSignupVisible);
     };
+
 
     return (
         <div>
@@ -59,110 +58,77 @@ const Navbar = () => {
                             </button>
                         </li>
 
+                        <li>
+                            <AnimatedThemeToggler />
+                        </li>
+                    </ul>
 
-                        return (
-                        <div>
-                            <header className="bg-[#48A6A7] shadow-md text-shadow-cyan-50 text-amber-50 py-7">
-                                <nav className="flex md:flex justify-between items-center gap-12 px-6">
-                                    <div className="md:flex gap-3.5">
-                                        <Link to="/">
-                                            <img
-                                                src="/Gadme.png"
-                                                alt="gadme-logo"
-                                                width="30px"
-                                                className="m-2"
-                                            />
-                                        </Link>
-                                        <p className="md:text-4xl md:font-semibold md:uppercase md:hover:text-green-800">
-                                            Gad-me
-                                        </p>
-                                    </div>
-                                    <ul className="hidden md:flex gap-7 m-2">
-                                        <li className="md:hover:text-amber-300">
-                                            <Link to="/">Home</Link>
-                                        </li>
-                                        <li className="md:hover:text-amber-300">
-                                            <Link to="/productlists">Products</Link>
-                                        </li>
-                                        <li className="md:hover:text-amber-300">About</li>
-                                        <li className="md:hover:text-amber-300">Contact</li>
-                                        <li className="md:hover:text-amber-300">
-                                            <button onClick={toggleLoginAndSignup}>
-                                                <BsPersonFill className="text-2xl " />
-                                            </button>
-                                        </li>
+                    <ul className="flex gap-4 m-2 md:hidden">
+                        <li>
+                            <button onClick={toggleSearch}>
+                                <BsSearch className="text-2xl " />
+                            </button>
+                        </li>
 
-                                        <li>
-                                            <AnimatedThemeToggler />
-                                        </li>
-                                    </ul>
+                        <li>
+                            <Link to="/productlists">
+                                <BsBasket className="text-2xl " />
+                            </Link>
+                        </li>
 
-                                    <ul className="flex gap-4 m-2 md:hidden">
-                                        <li>
-                                            <button onClick={toggleSearch}>
-                                                <BsSearch className="text-2xl " />
-                                            </button>
-                                        </li>
+                        <li>
+                            <button
+                                onClick={toggleLoginAndSignup}
+                                className="flex flex-row">
+                                <BsPersonFill className="text-2xl " />
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+                {isSearchVisible && <Searchbar />}
+                {isLoginVisible && <LoginForm />}
+                {isSignupVisible && <SignupForm />}
 
-                                        <li>
-                                            <Link to="/productlists">
-                                                <BsBasket className="text-2xl " />
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <button
-                                                onClick={toggleLoginAndSignup}
-                                                className="flex flex-row">
-                                                <BsPersonFill className="text-2xl " />
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </nav>
-                                {isSearchVisible && <Searchbar />}
-                                {isLoginVisible && <LoginForm />}
-                                {isSignupVisible && <SignupForm />}
-
-                                <div className="bg-white flex flex-row w-auto sm:w-full gap-5">
-                                    <Link to="/">
-                                        <img
-                                            src="/notebookpic.jpg"
-                                            alt="gadme-logo"
-                                            className="rounded-xl shadow-md m-2 "
-                                        />
-                                    </Link>
-                                    <Link to="/">
-                                        <img
-                                            src="/notebookpic.jpg"
-                                            alt="gadme-logo"
-                                            className="rounded-xl shadow-md m-2"
-                                        />
-                                    </Link>
-                                    <Link to="/">
-                                        <img
-                                            src="/notebookpic.jpg"
-                                            alt="gadme-logo"
-                                            className="rounded-xl shadow-md m-2"
-                                        />
-                                    </Link>
-                                    <Link to="/">
-                                        <img
-                                            src="/notebookpic.jpg"
-                                            alt="gadme-logo"
-                                            className="rounded-xl shadow-md m-2"
-                                        />
-                                    </Link>
-                                    <Link to="/">
-                                        <img
-                                            src="/notebookpic.jpg"
-                                            alt="gadme-logo"
-                                            className="rounded-xl shadow-md m-2"
-                                        />
-                                    </Link>
-                                </div>
-                            </header>
-                        </div>
-                        );
+                <div className="bg-white flex flex-row w-auto sm:w-full gap-5">
+                    <Link to="/">
+                        <img
+                            src="/notebookpic.jpg"
+                            alt="gadme-logo"
+                            className="rounded-xl shadow-md m-2 "
+                        />
+                    </Link>
+                    <Link to="/">
+                        <img
+                            src="/notebookpic.jpg"
+                            alt="gadme-logo"
+                            className="rounded-xl shadow-md m-2"
+                        />
+                    </Link>
+                    <Link to="/">
+                        <img
+                            src="/notebookpic.jpg"
+                            alt="gadme-logo"
+                            className="rounded-xl shadow-md m-2"
+                        />
+                    </Link>
+                    <Link to="/">
+                        <img
+                            src="/notebookpic.jpg"
+                            alt="gadme-logo"
+                            className="rounded-xl shadow-md m-2"
+                        />
+                    </Link>
+                    <Link to="/">
+                        <img
+                            src="/notebookpic.jpg"
+                            alt="gadme-logo"
+                            className="rounded-xl shadow-md m-2"
+                        />
+                    </Link>
+                </div>
+            </header>
+        </div>
+    );
 };
 
-                        export default Navbar;
+export default Navbar;
