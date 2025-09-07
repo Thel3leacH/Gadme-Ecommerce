02d737501f8eebe11db4362669332369a674a5b2
suvitcha <boom.suvitcha@gmail.com>
2025-09-07 21:43:01 +0700

    fix:Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/landingpage/Navbar";
+import Footer from "../components/landingpage/Footer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
