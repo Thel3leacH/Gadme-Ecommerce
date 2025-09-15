38ce1fdc848ec1d40dd347594d756055118e733b
suvitcha <boom.suvitcha@gmail.com>
2025-09-16 05:15:40 +0700

    add reg

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { ProductsProvider } from "@/context/ProductsContext";
 import { CartProvider } from "@/context/CartContext";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ProductsProvider>
       <CartProvider>
         <App />
+        <Toaster position="top-center" />
       </CartProvider>
     </ProductsProvider>
   </StrictMode>
